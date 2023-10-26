06be8106102823f44222f7f36e48b0b6c80ca558
Logan O'Connell <logan.w.oconnell@gmail.com>
2023-10-26 00:53:04 -0700

    Update

--- docker.sh
+++ docker.sh
@@ -4,5 +4,5 @@ set -ex
 VERSION="0.0.1"
 
 docker build -t tokamak-server:$VERSION .
-docker tag mutex:$VERSION loganoconnell/tokamak-server:$VERSION
+docker tag tokamak-server:$VERSION loganoconnell/tokamak-server:$VERSION
 docker image push loganoconnell/tokamak-server:$VERSION
